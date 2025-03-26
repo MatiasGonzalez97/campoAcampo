5d29ba0916e5f87f34e28d54e94de261fd40375b
mati gonzalez <matigonzalez@provart.com.ar>
2025-03-26 20:59:27 -0300

    deleted unused import

--- src/productos/productos.service.ts
+++ src/productos/productos.service.ts
@@ -2,7 +2,6 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Producto } from './entities/producto.entity';
-import { plainToInstance } from 'class-transformer';
 
 @Injectable()
 export class ProductosService {
